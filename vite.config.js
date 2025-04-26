f7389a2e61bff3bdff31c3f978a8e6b2fccf8355
Naren J <nikk.projects@gmail.com>
2025-04-26 17:05:42 +0530

    Vite config updated

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import path from "path";
 
 export default defineConfig({
-	root: "/oilfolexpro/",
+	root: ".",
 	build: {
 		outDir: "build",
 		sourcemap: true,
